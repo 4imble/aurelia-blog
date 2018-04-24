3932468546e550182506c6f321f52f4c9cb5ee5f
Simon Lawrence <simon.lawrence@rsmuk.com>
2018-04-24 20:18:47 +0100

    Updated Components: Settings

---
+++ src/components/settings/settings-component.ts
@@ -0,0 +1,19 @@
+import { autoinject } from 'aurelia-dependency-injection';
+import { Router } from 'aurelia-router';
+import { UserService } from '../../shared/services/user-service';
+import { SharedState } from '../../shared/state/shared-state';
+
+@autoinject()
+export class SettingsComponent {
+
+  constructor(private userService: UserService, private sharedState: SharedState, private router: Router) {  }
+
+  update() {
+    this.userService.update(this.sharedState.currentUser);
+  }
+
+  logout() {
+    this.userService.purgeAuth();
+    this.router.navigateToRoute('home');
+  }
+}
